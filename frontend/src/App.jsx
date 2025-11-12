fff22fe1fd6cd6481720f79189f5414f22d49f86
NurAlbina <148437547+NurAlbina@users.noreply.github.com>
2025-11-12 08:40:33 +0300

    FIX: Korumalı rota (ProtectedRoute) eklendi ve App.jsx'teki import hatası düzeltildi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
-import Dashboard from "./pages/Dashboard"
-
-
+//import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import DashboardPage from './pages/Dashboard.jsx';
 function App() {
 
   return (
@@ -12,7 +12,14 @@ function App() {
       <Routes>
         <Route path = "/login" element = {<LoginPage/>}/>
         <Route path = "/register" element = {<RegisterPage/>}/>
-        <Route path = "/dashboard" element = {<Dashboard/>}/>
+       <Route 
+  path="/dashboard" 
+  element={
+    <ProtectedRoute>
+      <DashboardPage />
+    </ProtectedRoute>
+  } 
+/>
         <Route path = "/" element = {<Navigate to = "/login" replace/>}/> {/* Ana sayfa olarak login sayfasına yönlendirir */}
       </Routes>
     </Router>
